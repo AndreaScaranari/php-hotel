82ca9a52508e83762001f55f439e82de869246e8
AndreaScaranari <andrea.scaranari.93@gmail.com>
2024-02-20 16:16:23 +0100

    table_no_graphic

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php
     include 'data/data.php';
-
-    var_dump($hotels);
+    // var_dump($hotels);
 ?>
 
 <!DOCTYPE html>
@@ -9,9 +8,33 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>BoHotels - La miglior scelta ovunque</title>
 </head>
 <body>
-    
+    <main>
+        <!-- Tabella -->
+        <table>
+            <!-- Intestazione tabella -->
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Description</th>
+                    <th>Parking</th>
+                    <th>Vote</th>
+                    <th>Distance to Center</th>
+                </tr>
+            </thead>
+            <!-- Corpo tabella -->
+            <tbody>
+                <?php foreach ($hotels as $hotel) :?>
+                <tr>
+                    <?php foreach ($hotel as $property) :?>
+                    <td><?= $property ?></td>
+                    <?php endforeach ;?>
+                </tr>
+                <?php endforeach ;?>
+            </tbody>
+        </table>
+    </main>
 </body>
 </html>
