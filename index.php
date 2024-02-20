08b5492f6fe3b43a45a6acf93e6b1b1397b26902
AndreaScaranari <andrea.scaranari.93@gmail.com>
2024-02-20 16:59:13 +0100

    table_styled

--- index.php
+++ index.php
@@ -9,32 +9,47 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>BoHotels - La miglior scelta ovunque</title>
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+
+    <!-- Fontawesome Icons -->
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css' integrity='sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==' crossorigin='anonymous'/>
 </head>
 <body>
+    <header class="text-center py-3 mb-2">
+        <h1>BoHotel</h1>
+        <h3>La Miglior Scelta Ovunque</h3>
+    </header>
+
     <main>
+        <div class="container">
         <!-- Tabella -->
-        <table>
-            <!-- Intestazione tabella -->
-            <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>Description</th>
-                    <th>Parking</th>
-                    <th>Vote</th>
-                    <th>Distance to Center</th>
-                </tr>
-            </thead>
-            <!-- Corpo tabella -->
-            <tbody>
-                <?php foreach ($hotels as $hotel) :?>
-                <tr>
-                    <?php foreach ($hotel as $property) :?>
-                    <td><?= $property ?></td>
+            <table class="table table-striped table-hover text-center">
+                <!-- Intestazione tabella -->
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Description</th>
+                        <th>Parking</th>
+                        <th>Vote</th>
+                        <th>Distance to Center</th>
+                    </tr>
+                </thead>
+                <!-- Corpo tabella -->
+                <tbody>
+                    <?php foreach ($hotels as $hotel) :?>
+                    <tr>
+                        <td><?= $hotel['name'] ?></td>
+                        <td><?= $hotel['description'] ?></td>
+                        <td><?php echo $hotel['parking'] ? '<i class="fa-solid fa-circle-check text-success"></i>' : '<i class="fa-solid fa-circle-xmark text-danger"></i>' ?></td>
+                        <td><?= $hotel['vote'] ?></td>
+                        <td><?= $hotel['distance_to_center'] ?></td>
+                    </tr>
                     <?php endforeach ;?>
-                </tr>
-                <?php endforeach ;?>
-            </tbody>
-        </table>
+                </tbody>
+            </table>
+        </div>
     </main>
 </body>
 </html>
